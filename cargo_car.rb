d492af892c214785a729037aea8ae25bb27de38b
lele1957 <75094755+lele1957@users.noreply.github.com>
2021-05-03 22:03:36 +0300

    Add files via upload

---
+++ cargo_car.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative './company'
+require_relative './instance_counter'
+
+class CargoCar
+  include Company
+  include InstanceCounter
+
+  attr_reader :type, :id, :volume_filled
+
+  def initialize(id, _volume_filled)
+    @volume_value = volume_value
+    @volume_filled = 0
+    @id = id
+    @type = 'cargo'
+  end
+
+  def fill_volume(volume)
+    @volume_filled += volume if volume_left >= volume
+  end
+
+  def volume_left
+    @volume_value - volume_filled
+  end
+end
